23f3465c81dee17f27c6dcff557013ed5c76b571
hongichik <hongichik321@gmail.com>
2022-11-23 10:05:38 +0700

    test'

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -3,19 +3,17 @@
 @section('SeoConent')
     <meta name="description" content="{{ setting('home.description') }}" />
     <meta name="keywords" content="{{ setting('home.keyword') }}" />
-
+    <meta property="og:locale" content="vi_VN" />
+    <meta property="og:type" content="webhome" />
     <meta property="og:title" content="{{ setting('home.title') }}" />
     <meta property="og:description" content="{{ setting('home.description') }} " />
-    <meta property="og:url" content="{{ env('APP_URL').$_SERVER['REQUEST_URI'] }}" />
-    <meta property="og:image" content="{{ asset('storage') . '/' . str_replace('\\', '/', setting('home.logo_Menu')) }}" />
+    <meta property="og:url" content="{{ asset('storage') . '/' }}" />
+    <meta property="og:image" content="{{ asset('storage') .'/'.str_replace('\\','/',setting('home.logo_Menu')) }}" />
     <meta property="og:home_name" content="{{ setting('home.title') }}" />
+    <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="{{ setting('home.title') }}" />
     <meta name="twitter:description" content="{{ setting('home.description') }} " />
-    <meta name="twitter:image" content="{{ asset('storage') . '/' . str_replace('\\', '/',setting('home.logo_Menu')) }}" />
-    <meta name="title" content="{{ setting('home.title') }}" />
-    <meta name="thumbnail" content="{{ asset('storage') . '/' . str_replace('\\', '/', setting('home.logo_Menu')) }}" />
-    <meta property="og:image:secure_url"
-        content="{{ asset('storage') . '/' . str_replace('\\', '/',setting('home.logo_Menu')) }}" />
+    <meta name="twitter:image" content="{{ asset('storage') . '/' . setting('home.logo_Menu') }}" />
     <meta name="theme-color" content="#0086cd" />
 
     <title>{{ setting('home.title') }}</title>
@@ -34,14 +32,14 @@
                                 data-pause="true">
                                 <ol class="carousel-indicators m-0">
                                     <?php $key2 = 0; ?>
-                                    @foreach ($new_1->PostMany as $key => $new)
+                                    @foreach ($new_1->PostMany as $key=>$new)
                                         @if ($new->status == 'PUBLISHED')
                                             @if ($key2 == 0)
                                                 <?php $key2 = 1; ?>
-                                                <li data-target="#tintuc" data-slide-to="{{ $key - 1 }}"
-                                                    class="active" style="background-color: var(--blue-coler-1)"></li>
+                                                <li data-target="#tintuc" data-slide-to="{{ $key-1 }}" class="active"
+                                                    style="background-color: var(--blue-coler-1)"></li>
                                             @else
-                                                <li data-target="#tintuc" data-slide-to="{{ $key - 1 }}"
+                                                <li data-target="#tintuc" data-slide-to="{{ $key-1 }}"
                                                     style="background-color: var(--blue-coler-1)"></li>
                                             @endif
                                         @endif
