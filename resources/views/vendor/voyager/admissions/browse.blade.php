54a22cb7f4cfaefad510b926dcd883ebe5fb3d4b
hongichik <hongichik321@gmail.com>
2023-05-25 10:40:40 +0700

    update

--- resources/views/vendor/voyager/admissions/browse.blade.php
+++ resources/views/vendor/voyager/admissions/browse.blade.php
@@ -36,7 +36,7 @@
             @endif
         @endforeach
         @include('voyager::multilingual.language-selector')
-        <a href="admin/admissions/export" class="btn btn-info">
+        <a href="admissions/export" class="btn btn-info">
             <span>Xuất excel</span></span>
         </a>
     </div>
